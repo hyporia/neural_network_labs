29219152588f4f3d923c637b358e276a9f6bc657
Maxim <maximgalimov96@gmail.com>
2018-12-23 23:18:37 +0300

    real-time object recognitnion; cnn for classification

---
+++ CNN_4_classification/source/xception.py
@@ -0,0 +1,26 @@
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.xception import preprocess_input
+from keras.applications.xception import decode_predictions
+from keras.applications.xception import Xception
+
+model = Xception(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+for i in range(0, 5):
+    print('-----------------------------------------------')
+    # load an image from file
+    image = load_img('../resources/wolf_'+str(i+1)+'.jpg', target_size=(224, 224))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # predict the probability across all output classes
+    yhat = model.predict(image)
+    # convert the probabilities to class labels
+    label = decode_predictions(yhat)
+    for i in range(0, len(label[0])):
+        # retrieve the most likely result, e.g. highest probability
+        labelTemp = label[0][i]
+        # print the classification
+        print('%s (%.2f%%)\n' % (labelTemp[1], labelTemp[2]*100))
